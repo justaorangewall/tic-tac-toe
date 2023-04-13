fbdb74c1f6a414919bfee79805129ce7c7e1f724
justaorangewall <128050806+justaorangewall@users.noreply.github.com>
2023-04-13 03:55:12 -0400

    added gameBoard to DOM

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function randomXO () {
+  const tileContent = Math.random() < 0.5;
+  return tileContent ? 'X' : 'O';
+}
 const gameboard = (() => {
   const board = new Array(3).fill(new Array(3).fill(0));
 
@@ -9,10 +13,28 @@ const gameboard = (() => {
 })();
 
 const displayController = (() => {
+  const drawBoard = () => {
+    const gameDiv = document.querySelector('.gameArea');
+    const boardDiv = document.createElement('div');
+    boardDiv.classList.add('gameboard');
 
-  return {};
+    for (let i = 0; i < 3; i += 1) {
+      const boardRow = document.createElement('div');
+      boardRow.classList.add('boardRow');
+      for (let j = 0; j < 3; j += 1) {
+        const tileDiv = document.createElement('div');
+        tileDiv.classList.add('tile');
+        tileDiv.textContent = randomXO();
+        boardRow.appendChild(tileDiv);
+      }
+      boardDiv.appendChild(boardRow);
+    }
+    console.log(boardDiv);
+    console.log(gameDiv);
+    gameDiv.appendChild(boardDiv);
+  };
+  return { drawBoard };
 })();
-
 
 const Player = (playerid) => {
   const chooseTile = (playerid, col, row) =>{
@@ -21,6 +43,4 @@ const Player = (playerid) => {
   return { playerid };
 };
 gameboard.getTable();
-
-
-
+displayController.drawBoard();
