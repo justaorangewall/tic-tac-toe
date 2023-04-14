b392f7a2e04c30873095bc5e6bd250a3e1a8716e
justaorangewall <128050806+justaorangewall@users.noreply.github.com>
2023-04-14 09:33:27 -0400

    got new game button working

--- index.js
+++ index.js
@@ -12,10 +12,22 @@ const gameboard = (() => {
 
   const gTab = () => board;
   const getTurn = () => curTurn;
-  const setTurn = (num) => { curTurn = num; };
   const getWinStatus = () => winPlayer;
+
+  const setTurn = (num) => { curTurn = num; };
+  const resetWin = () => { winPlayer = 0; };
+  const setTable = () => {
+    board = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+  };
+
   const clearTable = () => {
-    board = new Array(3).fill(new Array(3).fill(0));
+    setTable();
+    setTurn(1);
+    resetWin(0);
   };
 
   const flipTurn = () => {
@@ -25,33 +37,36 @@ const gameboard = (() => {
     }
   };
   const checkWin = () => {
+    let winFlag = false;
     if (getWinStatus() === 0) {
       // check horizontal-line Win
       gTab().forEach((r) => {
-        if (r[0] === getTurn() && r[1] === getTurn() && r[2] === getTurn()) winPlayer = getTurn();
+        if (r[0] === getTurn() && r[1] === getTurn() && r[2] === getTurn()) {
+          winPlayer = getTurn();
+          winFlag = true;
+        }
       });
 
       // check vertical Win
       for (let i = 0; i < 3; i += 1) {
         if (gTab()[0][i] === getTurn() && gTab()[1][i] === getTurn() && gTab()[2][i] ===getTurn()) {
           winPlayer = getTurn();
+          winFlag = true;
         }
       }
 
       // check diagonal line
       if (gTab()[0][0] === getTurn() && gTab()[1][1] === getTurn() && gTab()[2][2] === getTurn()) {
         winPlayer = getTurn();
+        winFlag = true;
       }
 
       if (gTab()[0][2] === getTurn() && gTab()[1][1] === getTurn() && gTab()[2][0] === getTurn()) {
         winPlayer = getTurn();
-      }
-      if (winPlayer !== 0) {
-        console.log("YOU WIN");
-        const result = document.querySelector('.gameResult');
-        result.textContent = `Congratulation ${player1.getName()}, YOU won!`;
+        winFlag = true;
       }
     }
+    return winFlag;
   };
   return {
     flipTile, gTab, clearTable, flipTurn, getTurn, checkWin, getWinStatus,
@@ -64,23 +79,30 @@ const displayController = (() => {
 
     tileList.forEach((tile) => {
       tile.addEventListener('click', (event) => {
-        if (gameboard.getWinStatus() === 0) { // eslint-disable-next-line no-param-reassign
-          event.target.dataset.playerowned = gameboard.getTurn();
-          event.target.classList.add('claimed');
-          const tileRow = event.target.dataset.row;
-          const tileCol = event.target.dataset.column;
-          gameboard.flipTile(gameboard.getTurn(), tileRow, tileCol);
+        if (gameboard.getWinStatus() === 0) { // check if game is already over
+          if (event.target.classList.contains('claimed') === false) {
           // eslint-disable-next-line no-param-reassign
-          event.target.textContent = (gameboard.getTurn() === 1) ? 'X' : 'O';
-          gameboard.checkWin();
-          gameboard.flipTurn();
+            event.target.dataset.playerowned = gameboard.getTurn();
+            event.target.classList.add('claimed');
+            const tileRow = event.target.dataset.row;
+            const tileCol = event.target.dataset.column;
+            // eslint-disable-next-line no-param-reassign
+            event.target.textContent = (gameboard.getTurn() === 1) ? 'X' : 'O';
+            gameboard.flipTile(gameboard.getTurn(), tileRow, tileCol);
+            if (gameboard.checkWin() === true) {
+              if (gameboard.getWinStatus() === 1) document.querySelector('body > div.gameResult').textContent = `${player1.getName()} has won`;
+              else if (gameboard.getWinStatus() === 2) document.querySelector('body > div.gameResult').textContent = `${player2.getName()} has won`;
+            }
+            gameboard.flipTurn();
+          }
         }
-      }, { once: true });
+      });
     });
   };
 
   const drawBoard = () => {
     const gameDiv = document.querySelector('.gameArea');
+    gameDiv.textContent = '';
     const boardDiv = document.createElement('div');
     boardDiv.classList.add('gameboard');
 
@@ -92,18 +114,24 @@ const displayController = (() => {
         tileDiv.classList.add('tile');
         tileDiv.dataset.column = j;
         tileDiv.dataset.row = i;
-        // tileDiv.textContent = randomXO();
         boardRow.appendChild(tileDiv);
       }
       boardDiv.appendChild(boardRow);
     }
-    console.log(boardDiv);
-    console.log(gameDiv);
     gameDiv.appendChild(boardDiv);
     addTileEvent();
   };
+  const resetTable = () => {
+    const tileList = document.querySelectorAll('.tile');
 
-  return { drawBoard };
+    tileList.forEach((tile) => {
+      tile.classList.remove('claimed');
+      // eslint-disable-next-line no-param-reassign
+      tile.textContent = '';
+    });
+    document.querySelector('body > div.gameResult').textContent='';
+  };
+  return { drawBoard, resetTable };
 })();
 
 const Player = (name, playerid) => {
@@ -121,3 +149,9 @@ gameboard.gTab();
 displayController.drawBoard();
 const player1 = Player('P1', 1);
 const player2 = Player('P2', 2);
+
+document.querySelector('.newGameBtn').addEventListener('click', () => {
+  gameboard.clearTable();
+  displayController.resetTable();
+  console.log(gameboard.getWinStatus());
+});
